b69d76ce6215ec3f583cd5e140b8c36eb487a78f
priest210 <ken@priest210.com>
2014-02-07 18:18:15 -0600

    added increment

--- php/for.php
+++ php/for.php
@@ -13,13 +13,15 @@ fwrite(STDOUT, "Please enter a ending number. \n");
 
 	$ending_number = (intval(trim(fgets(STDIN))));	
 	
+fwrite(STDOUT, "Please choose a increment for the number count. \n");
 
+	$increment = (intval(trim(fgets(STDIN))));
 
-for($i = $starting_number; $i <= $ending_number; $i++){
+for($i = $starting_number; $i <= $ending_number; $i += $increment){
 	echo "$i \n";
 }
 
-
+//Refactor to allow user to choose increment. (count by 1, 2, 10, ...)
 
 
 
