276cbb767d4b20674ada45e85698961c4daaa41c
priest210 <ken@priest210.com>
2014-02-10 11:24:07 -0600

    foreach problem 2

--- php/foreach.php
+++ php/foreach.php
@@ -4,28 +4,8 @@ $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, 
 
 foreach ($things as $thing) {
 		
-		if (is_int($thing)) {
-			echo "$thing is a integar \n";
-		}
-
-		elseif (is_float($thing)) {
-			echo "$thing is a float \n";
-		}
-
-		elseif (is_bool($thing)) {
-			echo "\$thing is a boolean \n";
-		}
-
-		elseif (is_array($thing)) {
-			echo "\$thing is an array \n";
-		}
-
-		elseif (is_string($thing)) {
-			echo "$thing is a string \n";
-		}
-
-		elseif (is_null($thing)) {
-			echo "\$thing is null \n";
+		if (is_scalar($thing)) {
+			echo "$thing \n";
 		}
 		
 
