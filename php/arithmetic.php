14a5a2a731f62ddaa44d6b8b5543365296ca4e95
priest210 <ken@priest210.com>
2014-02-11 13:36:07 -0600

    added #2 & 3 assignment

--- php/arithmetic.php
+++ php/arithmetic.php
@@ -2,34 +2,61 @@
 
 
 function add($a, $b) {
-    echo $a + $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a + $b . "\n";
+    } else {
+    	echo "ERROR! Both numbers must be numeric \n";
+    }
 }
 
-add(10, 12);
+add(10, 'b');
 
 function subtract($a, $b) {
-    echo $a - $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a - $b . "\n";
+    } else {
+    	echo "ERROR! Both numbers must be numeric \n";
+    }
+    
 }
 
-subtract(75, 25);
+subtract(75, 'b');
 
 function multiply($a, $b) {
-    echo $a * $b . "\n";
+    if (is_numeric($a) && is_numeric($b)) {
+    	echo $a * $b . "\n";
+    } else {
+    	echo "ERROR! Both numbers must be numeric \n";
+    }
 }
 
-multiply(5, 25);
+multiply(5, 'b');
 
 function divide($a, $b) {
-    echo $a / $b . "\n";
+    if ($b == 0) {
+    	echo "Both numbers must be greater than 0 \n";
+    }
+    elseif (is_numeric($a) && is_numeric($b)) {
+    	echo $a / $b . "\n";
+    } else {
+    	echo "ERROR! Can\'t divide by zero \n";
+    }
 }
 
-divide(20, 2);
+divide(20, 0);
 
 
 function modulus($a, $b) {
-	echo $a % $b . "\n";
+	if ($b == 0) {
+    	echo "Both numbers must be greater than 0 \n";
+    }
+    elseif (is_numeric($a) && is_numeric($b)) {
+    	echo $a / $b . "\n";
+    } else {
+    	echo "ERROR! Can\'t divide by zero \n";
+    }
 }
 
-modulus(10, 3);
+modulus(10, 0);
 
 ?>
