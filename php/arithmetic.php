865493f2dd6ed430e28bec098ad6611073010640
priest210 <ken@priest210.com>
2014-02-11 11:11:40 -0600

    added arithmetic function file

---
+++ php/arithmetic.php
@@ -0,0 +1,35 @@
+<?php
+
+
+function add($a, $b) {
+    echo $a + $b . "\n";
+}
+
+add(10, 12);
+
+function subtract($a, $b) {
+    echo $a - $b . "\n";
+}
+
+subtract(75, 25);
+
+function multiply($a, $b) {
+    echo $a * $b . "\n";
+}
+
+multiply(5, 25);
+
+function divide($a, $b) {
+    echo $a / $b . "\n";
+}
+
+divide(20, 2);
+
+
+function modulus($a, $b) {
+	echo $a % $b . "\n";
+}
+
+modulus(10, 3);
+
+?>
