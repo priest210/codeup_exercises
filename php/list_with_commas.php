7d110e0ba62d089a56371ead3aaa34401ccadbe1
priest210 <ken@priest210.com>
2014-02-13 14:18:28 -0600

    created and made array

---
+++ php/list_with_commas.php
@@ -0,0 +1,17 @@
+<?php
+
+$physicists_string = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
+
+
+$physicists_array = explode(', ', $physicists_string);
+
+print_r($physicists_array);
+
+	
+
+	// function array_sentence($cut, $paste) {
+		
+	// 	last_item = array_pop($physicists_string);
+
+
+	// }
