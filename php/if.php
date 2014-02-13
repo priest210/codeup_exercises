cccbb8d09d4fcd9eda7ee022abcc1fbc406b0736
priest210 <ken@priest210.com>
2014-02-13 17:50:18 -0600

    did some practice with this file

--- php/if.php
+++ php/if.php
@@ -4,35 +4,39 @@ $b = 10;
 $c = '10';
 
 if ($a < $b) {
-	//output the appropriate result
-	echo "$a is less than $b\n";
-}	else {
-    // output the appropriate result
-    echo "$b is greater than $a\n";
+    echo "$a is less than $b \n";
+}
+
+else {
+    echo "$a is greater than $b \n";
 }
 
 if ($b >= $c) {
-    // output the appropriate result
-    echo "$b is greater than or equal to $c\n";
-}	else {
-    // output the appropriate result
-    echo "$b is less than or equal to $c\n";
+    echo "$b is greater than or equal to $c \n";
+}
+
+else {
+    echo "$b is less than or equal to $c \n";
 }
 
 if ($b === $c) {
-    // output the appropriate result
-    echo "$b is identical $c\n";
+    echo "$b is identical to $c";
 }
-	elseif ($b == $c){
-    // output the appropriate result
-    echo "$b is equal to $c\n";
+
+elseif ($b == $c) {
+    echo "$b is equal to $c";
 }
-	elseif ($b !== $c){
-    echo "$b is not identical to $c\n";
+
+elseif ($b !== $c) {
+    echo "$b is not identical to $c";
 }
-	else {
-    echo "$b is not equal to $c\n";
+
+else {
+    echo "$b is not equal to $c";
 }
+
+
+
 
 ?>
 
