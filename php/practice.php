1fc2cdbdaa13f4795fe06ea00fd63b1b9f471f93
priest210 <ken@priest210.com>
2014-02-14 17:00:39 -0600

    redo for practice

---
+++ php/practice.php
@@ -0,0 +1,35 @@
+
+<?php
+
+function add($a, $b) {
+    echo $a + $b ."\n"; 
+
+}
+
+add(1, 2);
+
+function subtract($a, $b) {
+    echo $a - $b ."\n";
+}
+
+subtract(6, 3); 
+
+function multiply($a, $b) {
+    echo $a * $b ."\n";
+}
+
+multiply(3, 1);
+
+function divide($a, $b) {
+    echo $a / $b ."\n";
+}
+
+divide(9, 3);
+
+function modulus($a, $b) {
+	echo $a % $b ."\n";
+}
+
+modulus(93, 30);
+
+
