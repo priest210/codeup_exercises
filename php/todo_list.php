c98c5212dd01b9b8092735bc6b4f4b1b0d1a5ff9
priest210 <ken@priest210.com>
2014-02-14 09:56:32 -0600

    Recreating to do list

--- php/todo_list.php
+++ php/todo_list.php
@@ -1,45 +1,67 @@
 <?php
 
+// Update the code to allow upper and lowercase inputs from user for all menu items. 
+
+// Test adding, removing, and quiting.
+
+// Update the program to start numbering the list with 1 instead of 0. 
+
+// Make sure remove still works as expected.
+
+
 
 // Create array to hold list of todo items
 $items = array();
+
+
 
 // The loop!
 do {
     // Iterate through list items
     foreach ($items as $key => $item) {
         // Display each item and a newline
-        $key++;
         echo "[{$key}] {$item}\n";
     }
 
+    
     // Show the menu options
     echo '(N)ew item, (R)emove item, (Q)uit : ';
 
+    
     // Get the input from user
     // Use trim() to remove whitespace and newlines
-    $input = strtoupper(trim(fgets(STDIN)));
+    $input = trim(fgets(STDIN));
+
     
+
     // Check for actionable input
     if ($input == 'N') {
-        // Ask for entry
         echo 'Enter item: ';
-        // Add entry to list array
         $items[] = trim(fgets(STDIN));
-    } elseif ($input == 'R') {
-        // Remove which item?
+    } 
+    // When I add [E]dit existing item option
+    // Add [E]dit to menu above
+    // elseif ($input == 'E'){
+    //     echo 'Enter item to edit: ';
+    //     $key = trim(fgets(STDIN));
+    }
+
+
+    elseif ($input == 'R') {
         echo 'Enter item number to remove: ';
-        // Get array key
         $key = trim(fgets(STDIN));
-        // Remove from array
-        $key = $key -1;
         unset($items[$key]);
     }
+
+
 // Exit when input is (Q)uit
 } while ($input != 'Q');
 
+
+
 // Say Goodbye!
 echo "Goodbye!\n";
+
 
 // Exit with 0 errors
 exit(0);
