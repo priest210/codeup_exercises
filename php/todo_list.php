c89077171dd2562a468c9062ad60569955a31e29
priest210 <ken@priest210.com>
2014-02-15 16:12:15 -0600

    added list and get functions.  made a functions list

--- php/todo_list.php
+++ php/todo_list.php
@@ -1,39 +1,57 @@
 <?php
 
-// Update the code to allow upper and lowercase inputs from user for all menu items. 
-
-// Test adding, removing, and quiting.
-
-// Update the program to start numbering the list with 1 instead of 0. 
-
-// Make sure remove still works as expected.
 
 
-
-// Create array to hold list of todo items
 $items = array();
 
 
 
-// The loop!
-do {
-    // Iterate through list items
-    foreach ($items as $key => $item) {
-        // Display each item and a newline
-        echo "[{$key}] {$item}\n";
-    }
+function list_items($list) {
 
+
+    //created $listed in order to append a value inside foreach
+    $listed = "";
+
+    foreach ($items as $key => $item) {
+        
+        $key +=1;
+        
+        $listed .= "[{$key}] {$item}\n";
+    
+    }
+    
+    return $listed;
+
+}
+    
+    // Function to get the input from user
+
+function get_input($upper = FALSE) {
+
+            // Get STDIN, strip whitespace and newlines
+    $input = trim(fgets(STDIN)));
+    
+    if ($input === TRUE) {
+        
+            // Convert to uppercase if $upper is true
+        return strtoupper($input);        
+    }
+        else {
+            
+                // Return filtered STDIN input
+            return $input;
+        }
+
+}
+    
+// Loop!
+
+do {
     
     // Show the menu options
-    echo '(N)ew item, (R)emove item, (Q)uit : ';
+    echo 'Please choose option: (N)ew item, (R)emove item, (Q)uit : ';
 
     
-    // Get the input from user
-    // Use trim() to remove whitespace and newlines
-    $input = trim(fgets(STDIN));
-
-    
-
     // Check for actionable input
     if ($input == 'N') {
         echo 'Enter item: ';
@@ -44,13 +62,15 @@ do {
     // elseif ($input == 'E'){
     //     echo 'Enter item to edit: ';
     //     $key = trim(fgets(STDIN));
-    }
 
+
+    // array_values is how it reorders array
 
     elseif ($input == 'R') {
         echo 'Enter item number to remove: ';
         $key = trim(fgets(STDIN));
-        unset($items[$key]);
+        unset($items[$key -1]);
+        $items = array_values($items);
     }
 
 
